484d8c1007e8745505bb8f873fcf0c325d0afe5f
Romain🔥 <romaintibo6@gmail.com>
2024-09-27 20:45:59 +0200

    feat(icons): add settings icon

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,5 +1,6 @@
-export { HomeIcon } from './HomeIcon';
-export { UserIcon } from './UserIcon';
-export { DashboardIcon } from './Dashboardcon';
-export { GitHubIcon } from './GitHubIcon';
-export { LogoutIcon } from './LogoutIcon';
+export { HomeIcon } from "./HomeIcon";
+export { UserIcon } from "./UserIcon";
+export { DashboardIcon } from "./Dashboardcon";
+export { GitHubIcon } from "./GitHubIcon";
+export { LogoutIcon } from "./LogoutIcon";
+export { SettingsIcon } from "./SettingsIcon";
