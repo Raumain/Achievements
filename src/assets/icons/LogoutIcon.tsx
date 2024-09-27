c1a57da7d4af45deeb2d5b624f0e1fb3368bec33
Romain🔥 <romaintibo6@gmail.com>
2024-09-27 20:40:42 +0200

    feat(icons)

---
+++ src/assets/icons/LogoutIcon.tsx
@@ -0,0 +1,18 @@
+export const LogoutIcon = () => (
+	<svg
+		width="20"
+		height="20"
+		viewBox="0 0 15 15"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<title>Sign Out</title>
+		<path
+			d="M13.5 7.5L10.5 10.5M13.5 7.5L10.5 4.5M13.5 7.5H4M8 13.5H1.5V1.5H8"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,5 @@
+export { HomeIcon } from './HomeIcon';
+export { UserIcon } from './UserIcon';
+export { DashboardIcon } from './Dashboardcon';
+export { GitHubIcon } from './GitHubIcon';
+export { LogoutIcon } from './LogoutIcon';
