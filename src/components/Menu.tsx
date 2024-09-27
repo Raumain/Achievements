30713ef1ee375d79be9c4a4b75b6130abf68bbed
Romain🔥 <romaintibo6@gmail.com>
2024-09-27 20:46:36 +0200

    feat(component): add Menu component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,30 @@
+import { Tab } from "./Tab";
+import {
+	DashboardIcon,
+	HomeIcon,
+	SettingsIcon,
+	UserIcon,
+} from "../assets/icons";
+import { useLocation } from "@tanstack/react-router";
+export const Menu = () => {
+	const pathname = useLocation().pathname;
+	return (
+		<div className="top-4 left-4 z-50 fixed">
+			<div className="gap-1 bg-base-200 shadow-lg p-2 sho tabs tabs-boxed">
+				<Tab link="/" icon={<HomeIcon />} text="Home" />
+				{pathname.includes("/settings") ? (
+					<>
+						<Tab link="/settings/user" icon={<UserIcon />} text="User" />
+						<Tab
+							link="/settings/dashboard"
+							icon={<DashboardIcon />}
+							text="Dashboard"
+						/>
+					</>
+				) : (
+					<Tab link="/settings/user" icon={<SettingsIcon />} text="Settings" />
+				)}
+			</div>
+		</div>
+	);
+};
