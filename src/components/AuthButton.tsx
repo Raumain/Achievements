45d4f79d3132305f7e5155748072a0625f7542e9
Romain🔥 <romaintibo6@gmail.com>
2024-09-27 20:47:56 +0200

    feat(component): add AuthButton (replace login/logout btn)

---
+++ src/components/AuthButton.tsx
@@ -0,0 +1,29 @@
+import { useAuthActions } from "@convex-dev/auth/react";
+import { useState } from "react";
+import { LogoutIcon, GitHubIcon } from "../assets/icons/";
+
+export function AuthButton({ isLogged }: { isLogged: boolean }) {
+	const { signIn, signOut } = useAuthActions();
+	const [isBusy, setIsBusy] = useState(false);
+
+	return (
+		<button
+			type="button"
+			className="top-2 right-4 fixed btn"
+			disabled={isBusy}
+			onClick={() => {
+				setIsBusy(true);
+				isLogged ? void signOut() : void signIn("github");
+			}}
+		>
+			{isBusy ? (
+				<span className="loading loading-spinner" />
+			) : isLogged ? (
+				<LogoutIcon />
+			) : (
+				<GitHubIcon />
+			)}
+			{isLogged ? "Sign Out" : "Sign In with GitHub"}
+		</button>
+	);
+}
