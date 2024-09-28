0a49a2d66c994883a577d43be43cc609de2642cf
Romain🔥 <romaintibo6@gmail.com>
2024-09-28 21:45:10 +0200

    update(achievements): update schema

--- convex/schemas/achievements.ts
+++ convex/schemas/achievements.ts
@@ -6,9 +6,10 @@ export const achievements = {
 		name: v.string(),
 		description: v.string(),
 		category: v.optional(v.string()),
-		timeInterval: v.string(),
-		from: v.string(),
-		to: v.string(),
+		timeInterval: v.union(v.literal("day"), v.literal("week"), v.literal("month")),
+		startDate: v.string(), // ISO date string
+		endDate: v.string(), // ISO date string
+		totalBoxes: v.number(),
 		boxColor: v.array(v.string()),
 		createdBy: v.string(),
 		updatedAt: v.string(),
