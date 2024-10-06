7d31e6f41bf6ee07f3ac3f1e2b8d8f3c7fa951d6
Romain🔥 <romaintibo6@gmail.com>
2024-10-06 21:41:22 +0200

    feat(boxes): add handlers

--- convex/handlers/boxes.ts
+++ convex/handlers/boxes.ts
@@ -10,6 +10,35 @@ export const getAll = query({
 			.collect();
 	},
 });
+export const getFive = query({
+	args: { achievementId: v.id("achievements") },
+	handler: async (ctx, args) => {
+		const today = new Date().toISOString().split("T")[0];
+
+		const pastAndTodayBoxes = await ctx.db
+			.query("boxes")
+			.filter((q) => q.eq(q.field("achievementId"), args.achievementId))
+			.filter((q) => q.lte(q.field("date"), today))
+			.order("desc")
+			.take(5);
+
+		let totalBoxes: typeof pastAndTodayBoxes = [];
+
+		if (pastAndTodayBoxes.length === 5) {
+			totalBoxes = pastAndTodayBoxes.reverse();
+		} else {
+			const futureBoxes = await ctx.db
+				.query("boxes")
+				.filter((q) => q.eq(q.field("achievementId"), args.achievementId))
+				.filter((q) => q.gt(q.field("date"), today))
+				.order("asc")
+				.take(5 - pastAndTodayBoxes.length);
+
+			totalBoxes = [...pastAndTodayBoxes.reverse(), ...futureBoxes];
+		}
+		return totalBoxes;
+	},
+});
 
 export const create = mutation({
 	args: {
@@ -23,5 +52,18 @@ export const create = mutation({
 	},
 	handler: async (ctx, args) => {
 		return args.boxes.map(async (box) => await ctx.db.insert("boxes", box));
+	},
+});
+
+export const update = mutation({
+	args: {
+		id: v.id("boxes"),
+		box: v.object({
+			color: v.optional(v.string()),
+			content: v.optional(v.string()),
+		}),
+	},
+	handler: async (ctx, args) => {
+		return await ctx.db.patch(args.id, args.box);
 	},
 });
