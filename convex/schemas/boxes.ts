f328d79bb26904ae9afd1b22d8aed14b6dbb2b3c
Romain🔥 <romaintibo6@gmail.com>
2024-10-06 21:41:02 +0200

    fix(boxes): add optional content

--- convex/schemas/boxes.ts
+++ convex/schemas/boxes.ts
@@ -4,6 +4,7 @@ import { v } from "convex/values";
 export const boxes = {
 	boxes: defineTable({
 		color: v.string(),
+		content: v.optional(v.string()),
 		date: v.string(),
 		achievementId: v.id("achievements"),
 	}),
